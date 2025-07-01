7e37a63ce363a01917fdacc57a23ad80a7950767
Prominance <prominencek77@gmail.com>
2025-07-01 17:39:50 +0200

    fix: update on return url and styles

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRouter from './router';
 import { Toaster } from 'react-hot-toast';
--- pages/SettingsPage.tsx
+++ pages/SettingsPage.tsx
@@ -1,6 +1,6 @@
 
 
-import React from 'react';
+
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
